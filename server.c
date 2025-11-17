9ffc9013d0cae6c7b52429e8e54eb84b01c873b5
mirunamm <147244152+mirunamm@users.noreply.github.com>
2025-11-17 19:14:52 +0200

    Update server.c

--- server.c
+++ server.c
@@ -8,116 +8,109 @@ typedef struct {
     char p2[32];
 } jucatori;
 
-char board[9];
+char tabla[9];
 
-void init_board() {
+void init_tabla() {
     for (int i = 0; i < 9; i++)
-        board[i] = ' ';
+        tabla[i] = ' ';
 }
 
-void print_board() {
+void afiseaza_tabla() {
     printf("\n");
-    printf(" %c | %c | %c\n", board[0], board[1], board[2]);
-    printf("---+---+---\n");
-    printf(" %c | %c | %c\n", board[3], board[4], board[5]);
-    printf("---+---+---\n");
-    printf(" %c | %c | %c\n", board[6], board[7], board[8]);
-    printf("\n");
+    for (int i = 0; i < 9; i++) {
+        if (tabla[i] == ' ')
+            printf(" %d ", i);  
+        else
+            printf(" %c ", tabla[i]); 
+
+        if (i % 3 != 2)
+            printf("|");
+        else if (i != 8)
+            printf("\n---+---+---\n");
+    }
+    printf("\n\n");
 }
 
-int check_win() {
-    
-    if (board[0]==board[1] && board[1]==board[2] && board[0]!=' ')
-        return 1;
-    if (board[3]==board[4] && board[4]==board[5] && board[3]!=' ')
-        return 1;
-    if (board[6]==board[7] && board[7]==board[8] && board[6]!=' ')
-        return 1;
-
-
-    if (board[0]==board[3] && board[3]==board[6] && board[0]!=' ')
-        return 1;
-    if (board[1]==board[4] && board[4]==board[7] && board[1]!=' ')
-        return 1;
-    if (board[2]==board[5] && board[5]==board[8] && board[2]!=' ')
-        return 1;
-
-
-    if (board[0]==board[4] && board[4]==board[8] && board[0]!=' ')
-        return 1;
-    if (board[2]==board[4] && board[4]==board[6] && board[2]!=' ')
-        return 1;
-
+int verificare_castig() {
+    if (tabla[0]==tabla[1] && tabla[1]==tabla[2] && tabla[0]!=' ') return 1;
+    if (tabla[3]==tabla[4] && tabla[4]==tabla[5] && tabla[3]!=' ') return 1;
+    if (tabla[6]==tabla[7] && tabla[7]==tabla[8] && tabla[6]!=' ') return 1;
+    if (tabla[0]==tabla[3] && tabla[3]==tabla[6] && tabla[0]!=' ') return 1;
+    if (tabla[1]==tabla[4] && tabla[4]==tabla[7] && tabla[1]!=' ') return 1;
+    if (tabla[2]==tabla[5] && tabla[5]==tabla[8] && tabla[2]!=' ') return 1;
+    if (tabla[0]==tabla[4] && tabla[4]==tabla[8] && tabla[0]!=' ') return 1;
+    if (tabla[2]==tabla[4] && tabla[4]==tabla[6] && tabla[2]!=' ') return 1;
     return 0;
 }
 
-void *game_thread(void *arg) {
-    jucatori *players = (jucatori *)arg;
-    int turn = 0;
+void *thread_joc(void *arg) {
+    jucatori *players = (jucatori*)arg;
     char cmd[32];
     int pos;
 
-    printf("\n=== Joc inceput intre %s (X) si %s (O) ===\n",
-           players->p1, players->p2);
-
-    init_board();
-    print_board();
+    printf("\n=== %s (X) vs %s (O) ===\n", players->p1, players->p2);
 
     while (1) {
+        int rand = 0;
+        init_tabla();
+        afiseaza_tabla();
 
-        if (turn == 0)
-            printf("%s (X), introdu comanda (MOVE x): ", players->p1);
-        if (turn == 1)
-            printf("%s (O), introdu comanda (MOVE x): ", players->p2);
+        while (1) {
+            if (rand == 0)
+                printf("%s (X), introdu comanda (MOVE n): ", players->p1);
+            else
+                printf("%s (O), introdu comanda (MOVE n): ", players->p2);
 
-        fgets(cmd, sizeof(cmd), stdin);
+            fgets(cmd, sizeof(cmd), stdin);
 
-        if (cmd[0]=='M' && cmd[1]=='O' && cmd[2]=='V' && cmd[3]=='E') {
+            if (cmd[0]=='M' && cmd[1]=='O' && cmd[2]=='V' && cmd[3]=='E') {
+                pos = atoi(&cmd[5]);
 
-            pos = atoi(&cmd[5]);
+                if (pos < 0) {
+                    printf("INVALID_MOVE (negativ)\n");
+                    continue;
+                }
+                if (pos > 8) {
+                    printf("INVALID_MOVE (prea mare)\n");
+                    continue;
+                }
+                if (tabla[pos] != ' ') {
+                    printf("INVALID_MOVE (ocupat)\n");
+                    continue;
+                }
 
- 
-            if (pos < 0) {
-                printf("SERVER: INVALID_MOVE (negativ)\n");
-                continue;
+                if (rand == 0) tabla[pos] = 'X';
+                else tabla[pos] = 'O';
+
+                afiseaza_tabla();
+
+                if (verificare_castig()) {
+                    if (rand == 0)
+                        printf("WIN pentru %s!\n", players->p1);
+                    else
+                        printf("WIN pentru %s!\n", players->p2);
+                    break;
+                }
+
+                rand = 1 - rand; 
             }
-            if (pos > 8) {
-                printf("SERVER: INVALID_MOVE (prea mare)\n");
-                continue;
+            else {
+                printf("Comanda necunoscuta. Foloseste MOVE poz\n");
             }
-            if (board[pos] != ' ') {
-                printf("SERVER: INVALID_MOVE (ocupat)\n");
-                continue;
-            }
-
-
-            if (turn == 0) board[pos] = 'X';
-            else board[pos] = 'O';
-
-            printf("Tabla ");
-            for (int i = 0; i < 9; i++)
-                printf("%c", board[i]);
-            printf("\n");
-
-            print_board();
-
-            if (check_win()) {
-                if (turn == 0)
-                    printf("CASTIGATOR: %s\n", players->p1);
-                else
-                    printf("CASTIGATOR: %s\n", players->p2);
-                break;
-            }
-
-            if (turn == 0) turn = 1;
-            else turn = 0;
         }
-        else {
-            printf("Comanda necunoscuta (foloseste MOVE poz)\n");
+
+        char rasp[8];
+        printf("Doriti rematch? (da/nu): ");
+        fgets(rasp, sizeof(rasp), stdin);
+
+        if (rasp[0]=='d' || rasp[0]=='D') {
+            printf("Pornim un nou meci intre aceiasi jucatori...\n");
+        } else {
+            printf("Joc terminat definitiv.\n");
+            break;
         }
     }
 
-    printf("=== Meci terminat ===\n");
     free(players);
     return NULL;
 }
@@ -127,21 +120,22 @@ int main() {
         char n1[32], n2[32];
 
         printf("\nIntrodu numele jucatorului 1: ");
-        fgets(n1, 32, stdin); n1[strcspn(n1, "\n")] = 0;
+        fgets(n1, 32, stdin);
+        n1[strcspn(n1, "\n")] = 0;
 
         printf("Introdu numele jucatorului 2: ");
-        fgets(n2, 32, stdin); n2[strcspn(n2, "\n")] = 0;
+        fgets(n2, 32, stdin);
+        n2[strcspn(n2, "\n")] = 0;
 
         jucatori *players = malloc(sizeof(jucatori));
         strcpy(players->p1, n1);
         strcpy(players->p2, n2);
 
         pthread_t tid;
-        pthread_create(&tid, NULL, game_thread, players);
-
+        pthread_create(&tid, NULL, thread_joc, players);
         pthread_join(tid, NULL);
 
-        printf("Meciul s-a încheiat, pornim altul...\n");
+        printf("\nJucatorii au terminat. Putem porni un alt joc cu alti jucatori.\n");
     }
 
     return 0;
